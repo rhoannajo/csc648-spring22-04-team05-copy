801b95a44bf4801a1bfcf4f985254ec0ee777786
Jia Li <55516461+michaelliftw@users.noreply.github.com>
2022-03-12 16:46:40 -0800

    Added profile icon and linked to login

--- csc648app/src/components/navbar/Navbar.js
+++ csc648app/src/components/navbar/Navbar.js
@@ -1,32 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './navbar.css'
+import "./navbar.css";
 
-import OnTask from "./OnTask-Logo.png"
-
+import OnTask from "./OnTask-Logo.png";
+import ProfileIcon from "./profileicon.png";
 
 const Navbar = () => {
-
-return (
+  return (
     <div>
-        <ul className="nav">
-            <li>
-                <Link to="/"> <img src={OnTask} />  </Link>
-            </li>
-            <li>
-                <input type="text" placeholder="Search" className="search"/> 
-            </li>
-            <li>
-                <Link to="/login" className="login">Login</Link>
-            </li>
-
-        </ul>
-
-
+      <ul className="nav">
+        <li>
+          <Link to="/">
+            {" "}
+            <img src={OnTask} />{" "}
+          </Link>
+        </li>
+        <li>
+          <input type="text" placeholder="Search" className="search" />
+        </li>
+        <li>
+          <Link to="/login" className="profileicon">
+            <img src={ProfileIcon} />
+          </Link>
+        </li>
+      </ul>
     </div>
-
-
-)
-}
+  );
+};
 
 export default Navbar;
